03c635a6ffcabfa56707fa69f8e9dad7e1d85064
Ronald DeSantis <Ronald.DeSantis.PMP@gmail.com>
2024-06-17 12:04:43 -0400

    Exercise: Setting up the HTML document

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import Nav from './Nav';
+
+function Footer() {
+    return (
+        <footer>
+            <img src='Vertical logo.png' alt='logo' height='100'/>
+            <Nav/>
+            <div>Contact</div>
+            <div>Social Media Links</div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from './Nav';
+
+function Header() {
+    return (
+        <header>
+            <img src='Horizontal logo.png' alt='logo' width='100'/>
+            <Nav/>
+        </header>
+    );
+};
+
+export default Header;
