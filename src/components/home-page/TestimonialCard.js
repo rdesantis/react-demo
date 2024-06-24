5b0465da43c4976dde48e2595fbdcf044a5c6912
Ronald DeSantis <Ronald.DeSantis.PMP@gmail.com>
2024-06-24 18:47:53 -0400

    Implemented testimonials

--- src/components/home-page/TestimonialCard.js
+++ src/components/home-page/TestimonialCard.js
@@ -3,7 +3,7 @@ const TestimonialCard = (props) => {
     const stars = (rating) => {
         return (
             <div className='stars'>
-                { Array(rating).fill(null).map(i => <img key={i} src='star.png' alt='star' />) }
+                { [...Array(rating).keys()].map(key => <img key={key} src='Little star.png' alt='star' />) }
             </div>
         );
     }
@@ -12,8 +12,10 @@ const TestimonialCard = (props) => {
     return (
         <div className='testimonial-card'>
             { stars(card.rating) }
-            <img className='testimonial-image' src={card.image} alt={card.name} />
-            <h2 className='testimonial-name'>{card.name}</h2>
+            <div className='testimonial-image'>
+                <img src={card.image} alt={card.name} />
+            </div>
+            <h6 className='testimonial-name'>{card.name}</h6>
             <p className='testimonial-review'>{card.review}</p>
         </div>
     );
--- src/components/home-page/Testimonials.js
+++ src/components/home-page/Testimonials.js
@@ -3,10 +3,28 @@ import TestimonialCard from "./TestimonialCard";
 
 const cards = [
     {
-        rating: 4,
+        rating: 5,
         image: 'people/young guy.png',
-        name: 'John Doe',
-        review: 'Good stuff'
+        name: 'John D',
+        review: 'Always fresh and delicious!'
+    },
+    {
+        rating: 4,
+        image: 'people/older woman.png',
+        name: 'Mary R',
+        review: 'My go-to restaurant for tasty Mediterannean dishes.'
+    },
+    {
+        rating: 4,
+        image: 'people/young woman.png',
+        name: 'Gloria G',
+        review: 'Never fails to bring a smile to my face!'
+    },
+    {
+        rating: 5,
+        image: 'people/other young guy.png',
+        name: 'Dieter M',
+        review: 'Scrumptious... capisci?'
     },
 ];
 
