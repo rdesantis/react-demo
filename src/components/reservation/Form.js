c441b9df8a23926bfecfe50b9a36f6214b16a1dc
Ronald DeSantis <Ronald.DeSantis.PMP@gmail.com>
2024-06-26 16:35:57 -0400

    Convert reservation form to React controlled form

--- src/components/reservation/Form.js
+++ src/components/reservation/Form.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 const ReservationForm = () => {
+    const [state, setState] = useState({
+        resDate: new Date().toISOString().substring(0, 10),
+        resTime: '17:00',
+        guests: 2,
+        occasion: '',
+    });
+
+    const handleChange = (key) => {
+        return (e) => {setState({...state, [key]: e.target.value})};
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // TODO
+        alert(`submitted ${state.resDate} ${state.resTime} ${state.guests} ${state.occasion}`);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="res-date">Choose date</label>
-            <input type="date" id="res-date" />
+            <input type="date" id="res-date" value={state.resDate} onChange={handleChange('resDate')} />
             <label htmlFor="res-time">Choose time</label>
-            <select id="res-time ">
+            <select id="res-time" value={state.resTime} onChange={handleChange('resTime')}>
                 <option>17:00</option>
                 <option>18:00</option>
                 <option>19:00</option>
@@ -13,9 +32,9 @@ const ReservationForm = () => {
                 <option>22:00</option>
             </select>
             <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" />
+            <input type="number" placeholder="2" min="1" max="10" id="guests" value={state.guests} onChange={handleChange('guests')} />
             <label htmlFor="occasion">Occasion</label>
-            <select id="occasion">
+            <select id="occasion" value={state.occasion} onChange={handleChange('occasion')}>
                 <option></option>
                 <option>Birthday</option>
                 <option>Anniversary</option>
