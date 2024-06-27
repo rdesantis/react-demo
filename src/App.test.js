c33830157da18aa2326ca500bc9edc31501b07be
Ronald DeSantis <Ronald.DeSantis.PMP@gmail.com>
2024-06-27 17:57:59 -0400

    Refactor to ad unit tests for initializeDates() and updateTimes()

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import BookingPage from './components/booking/BookingPage';
+import BookingPage, {initializeDates, updateTimes} from './components/booking/BookingPage';
 
-test('Renders the BookingPage button', () => {
+test('Renders the BookingPage header', () => {
   render(<BookingPage />);
   const headingElement = screen.getByText("Find a table for any occasion");
   expect(headingElement).toBeInTheDocument();
+});
+
+test('Initializes booking dates', () => {
+  expect(initializeDates()[0]).toBe(new Date().toLocaleDateString());
+});
+
+test('Updates booking times', () => {
+  expect(updateTimes(0)[0]).toBe('20:00');
 });
--- src/components/booking/BookingPage.js
+++ src/components/booking/BookingPage.js
@@ -4,33 +4,41 @@ import HeroHeader from "../HeroHeader";
 import MajorSection from "../MajorSection";
 import BookingForm from "./BookingForm";
 
+let today = new Date();
+let tomorrow = new Date(); tomorrow.setDate(today.getDate() + 1);
+let dayafter = new Date(); dayafter.setDate(tomorrow.getDate() + 1);
+
+// The following may be slow when called for many objects; see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+const dateImage = (d) => d.toLocaleDateString();
+
+today = dateImage(today);
+tomorrow = dateImage(tomorrow);
+dayafter = dateImage(dayafter);
+
+const slots = [
+    {
+        resDate: today,
+        resTimes: ['20:00', '21:00', '22:00', ]
+    },
+    {
+        resDate: tomorrow,
+        resTimes: ['18:00', '19:00', '21:00', '22:00', ]
+    },
+    {
+        resDate: dayafter,
+        resTimes: ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00', ]
+    },
+];
+
+export const initializeDates = () => {
+    return slots.map(slot => slot.resDate);
+}
+
+export const updateTimes = (dateIndex) => {
+    return slots[dateIndex].resTimes
+}
+
 const BookingPage = () => {
-    let today = new Date();
-    let tomorrow = new Date(); tomorrow.setDate(today.getDate() + 1);
-    let dayafter = new Date(); dayafter.setDate(tomorrow.getDate() + 1);
-
-    // The following may be slow when called for many objects; see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
-    const dateImage = (d) => d.toLocaleDateString();
-
-    today = dateImage(today);
-    tomorrow = dateImage(tomorrow);
-    dayafter = dateImage(dayafter);
-
-    const slots = [
-        {
-            resDate: today,
-            resTimes: ['20:00', '21:00', '22:00', ]
-        },
-        {
-            resDate: tomorrow,
-            resTimes: ['18:00', '19:00', '21:00', '22:00', ]
-        },
-        {
-            resDate: dayafter,
-            resTimes: ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00', ]
-        },
-    ];
-
     const reducer = (state, action) => {
         let newState = {...state, [action.key]: action.value};
         if (action.key === 'dateIndex') {
@@ -45,8 +53,8 @@ const BookingPage = () => {
         guests: 2,
         occasion: '',
 
-        availableDates: slots.map(slot => slot.resDate),
-        availableTimes: slots[0].resTimes,
+        availableDates: initializeDates(),
+        availableTimes: updateTimes(0),
     };
     const [state, dispatch] = useReducer(reducer, initialState);
 
